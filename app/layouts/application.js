9278a746cc3536b2a10877ab2faafeb3dd888823
Zack Siri <zack@codemy.net>
2017-11-15 18:32:28 +0700

    clean up pages

--- app/layouts/application.js
+++ app/layouts/application.js
@@ -1,28 +1,17 @@
-import React, { createElement } from 'react'
-import { routeNode } from 'react-router5';
-
-import * as pages from '../pages'
+import React from 'react'
+import Pages from '../pages'
 
 import './application.sass'
 
-const Page = routeNode('')((props) => {
-  const { route } = props;
-
-  const resource = route.name.split('.')[0]
-  const page = route.name.split('.')[-1] || 'collection'
-
-  return createElement(pages[resource][page])
-})
-
 class Application extends React.Component {
-  render() {
+  render() {    
     return (
       <div id='Application'>
         <div styleName='sidebar'>
           Sidebar
         </div>
         <div className='content'>
-          <Page />
+          <Pages />
         </div>
       </div>
     );
--- app/pages/index.js
+++ app/pages/index.js
@@ -1,5 +1,15 @@
-import * as posts from './posts';
+import { createElement } from 'react'
+import { routeNode } from 'react-router5'
 
-export { 
-  posts
-}
+import * as posts from './posts'
+
+const pages = { posts }
+
+export default routeNode('')((props) => {
+  const { route } = props;
+
+  const resource = route.name.split('.')[0]
+  const page = route.name.split('.')[-1] || 'collection'
+
+  return createElement(pages[resource][page])
+})
