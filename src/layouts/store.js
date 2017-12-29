e53779fe4bb0b0a06e434a104c8ddc8534836f79
Zack Siri <zack@codemy.net>
2017-12-29 10:34:23 +0700

    update documentation for layout store

--- src/layouts/store.js
+++ src/layouts/store.js
@@ -1,5 +1,8 @@
 import { observable, action } from 'mobx'
 
+// This store works in tandem with the Application layout
+// Any state you want to set on the Application can be passed
+// into the provider and changed by other components.
 const store = observable({
   modal: null,
   setModal: action(node =>
